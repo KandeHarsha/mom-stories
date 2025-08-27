cd77d2e22206ef63687498053a5f237908b41826
Vinay Harsha Vardhan Kande <kandevinayharshavardhan@gmail.com>
2025-08-27 07:56:56 +0000

    I want an header to always present. even when the content inside the bod

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -60,8 +60,8 @@ export default function AppLayout({ children }: AppLayoutProps) {
           </div>
         </nav>
       </aside>
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-64">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+      <div className="flex flex-col sm:pl-64">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:h-auto sm:bg-transparent sm:px-6">
            <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
@@ -99,7 +99,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
              <AppHeader />
            </div>
         </header>
-        <main className="flex-1 p-4 sm:px-6 sm:py-0">{children}</main>
+        <main className="flex-1 overflow-auto p-4 sm:p-6">{children}</main>
       </div>
     </div>
   );
