09fb4a0caf1c713c3184842c9a88bfe86ddb850d
Vinay Harsha Vardhan Kande <kandevinayharshavardhan@gmail.com>
2025-08-27 07:58:14 +0000

    No, When I scroll the content, the header is going out of screen

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -10,8 +10,6 @@ import {
   HeartHandshake,
   LayoutDashboard,
   Box,
-  Settings,
-  User,
   PanelLeft,
 } from 'lucide-react';
 import Link from 'next/link';
@@ -60,8 +58,8 @@ export default function AppLayout({ children }: AppLayoutProps) {
           </div>
         </nav>
       </aside>
-      <div className="flex flex-col sm:pl-64">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:h-auto sm:bg-transparent sm:px-6">
+      <div className="flex flex-col sm:pl-64 h-screen">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:px-6 shrink-0">
            <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
@@ -99,7 +97,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
              <AppHeader />
            </div>
         </header>
-        <main className="flex-1 overflow-auto p-4 sm:p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto p-4 sm:p-6">{children}</main>
       </div>
     </div>
   );
